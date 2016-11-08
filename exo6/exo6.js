44c17ecb7a5f250d50b6e9a2e40ab50c65844fe6
coda <roumejons@gmail.com>
2016-11-08 15:32:31 +0100

    exoterminer

---
+++ exo6/exo6.js
@@ -0,0 +1,6 @@
+function calcul() {
+var premier = document.getElementById('premier_nombre').value;
+var deuzieme = document.getElementById('deuxieme_nombre').value;
+
+alert(premier % deuzieme);
+};
